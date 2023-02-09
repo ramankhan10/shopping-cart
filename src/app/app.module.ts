fc0dd30cc48cf2785058ded51d05a13016ccac34
raman <khanahmadi.raman@yahoo.com>
2023-02-09 21:23:44 +0330

    create Product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingCartItemComponent } from './shopping-cart-item/shopping-cart-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingCartItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shopping-cart-item/shopping-cart-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shopping-item',
+  templateUrl: './shopping-cart-item.component.html',
+  styleUrls: ['./shopping-cart-item.component.scss']
+})
+export class ShoppingCartItemComponent {
+
+}
